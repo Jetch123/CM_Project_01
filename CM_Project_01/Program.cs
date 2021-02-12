e34dccaf2c44a4f91daf1c3ffab3c4634b9fa360
Jetch123 <67650698+Jetch123@users.noreply.github.com>
2021-02-12 18:50:29 +0100

    Add files via upload

---
+++ CM_Project_01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			while (true)
+			{
+				Console.WriteLine("\n\nType a number:");
+				Convertor.Convert(Console.ReadLine());
+
+				Console.Write("\nContinue? [Esc] to quit, [Enter] to continue: ");
+				if (Console.ReadKey().Key == ConsoleKey.Escape) break;
+			}
+		}
+	}
+}
